a382089a2dba24d428a2aa0b410bfff76a18fe45
Andrew Page <andrew.page32@gmail.com>
2017-06-06 00:46:15 -0400

    Test as JS

--- js/src/gqlQueries.js
+++ js/src/gqlQueries.js
@@ -1,3 +1,5 @@
+import gql from 'graphql-tag';
+
 "use strict";
 var graphql_tag_1 = require('graphql-tag');
 exports.gqlQueries = {
@@ -5,4 +7,32 @@ exports.gqlQueries = {
     employees: (_b = ["\n    query Employees($storeId: Int) {\n      store(id: $storeId) {\n        employees {\n          id\n          user {\n            id\n            fullName\n            email\n            phone\n            isActive\n          }\n        }\n      }\n    }\n  "], _b.raw = ["\n    query Employees($storeId: Int) {\n      store(id: $storeId) {\n        employees {\n          id\n          user {\n            id\n            fullName\n            email\n            phone\n            isActive\n          }\n        }\n      }\n    }\n  "], graphql_tag_1.default(_b))
 };
 var _a, _b;
+
+exports.gqlQueries1 = {
+  me: gql`
+    query Me {
+      me {
+        id,
+        firstName
+      }
+    }
+  `,
+  employees: gql`
+    query Employees($storeId: Int) {
+      store(id: $storeId) {
+        employees {
+          id
+          user {
+            id
+            fullName
+            email
+            phone
+            isActive
+          }
+        }
+      }
+    }
+  `
+};
+
 //# sourceMappingURL=gqlQueries.js.map
